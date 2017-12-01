12293db2ba61934a6964f41af232f850d1a3cc2d
Ricardo Mendes <ricardomlealjr@gmail.com>
2017-11-30 22:01:06 -0200

    k-média implementado para primeiro teste

--- k-media.py
+++ k-media.py
@@ -1,13 +1,16 @@
 #coding: utf-8
 
 import sys
+import math
 
 class Dado:
     def __init__(self, nome, d1, d2):
         self.nome = nome
         self.d1 = d1
         self.d2 = d2
-        self.cluster = -1
+        self.cluster_atual = -1
+        self.cluster_anterior = -1
+        self.dist = []
 
 vetor = []
 
@@ -47,30 +50,62 @@ for line1 in arquivo_cluster:
     c = int(line1.split()[1])
 
     # Inserindo no vetor de dados
-    vetor[i].cluster =  c
+    vetor[i].cluster_atual =  c
     i += 1
 
 # Inicializando variaveis para calculo de centroide
 campo_d1 = [0, 0]
 campo_d2 = [0, 0]
 qtdd = [0, 0]
+iteracao = 0
 
-# Calculo de centroide
-for i in range(len(vetor)):
-    if vetor[i].cluster == 0:
-        campo_d1[vetor[i].cluster] += vetor[i].d1
-        campo_d2[vetor[i].cluster] += vetor[i].d2
-        qtdd[0] += 1
+while True:
+    # Soma para o calculo de centroide
+    for i in range(len(vetor)):
+        if vetor[i].cluster_atual == 0:
+            campo_d1[vetor[i].cluster_atual] += vetor[i].d1
+            campo_d2[vetor[i].cluster_atual] += vetor[i].d2
+            qtdd[0] += 1
+        else:
+            campo_d1[vetor[i].cluster_atual] += vetor[i].d1
+            campo_d2[vetor[i].cluster_atual] += vetor[i].d2
+            qtdd[1] += 1
+
+    # Calculando a média do centroide
+    campo_d1[0] = campo_d1[0] / qtdd[0]
+    campo_d2[0] = campo_d2[0] / qtdd[0]
+    campo_d1[1] = campo_d1[1] / qtdd[1]
+    campo_d2[1] = campo_d2[1] / qtdd[1]
+
+    iteracao += 1
+    print 'interação:', iteracao
+    print 'Centroide 1: (', campo_d1[0], ',', campo_d2[0], ')'
+    print 'Centroide 2: (', campo_d1[1], ',', campo_d2[1], ')'
+
+    if iteracao == 1:
+        # Calculando a distância euclidiana de cada objeto aos centroides
+        for i in range(len(vetor)):
+            vetor[i].dist.insert(len(vetor[i].dist), math.sqrt(math.pow(vetor[i].d1 - campo_d1[0], 2) + math.pow(vetor[i].d2 - campo_d2[0], 2)))
+            vetor[i].dist.insert(len(vetor[i].dist), math.sqrt(math.pow(vetor[i].d1 - campo_d1[1], 2) + math.pow(vetor[i].d2 - campo_d2[1], 2)))
     else:
-        campo_d1[vetor[i].cluster] += vetor[i].d1
-        campo_d2[vetor[i].cluster] += vetor[i].d2
-        qtdd[1] += 1
+        for i in range(len(vetor)):
+            vetor[i].dist[0] = math.sqrt(math.pow(vetor[i].d1 - campo_d1[0], 2) + math.pow(vetor[i].d2 - campo_d2[0], 2))
+            vetor[i].dist[1] = math.sqrt(math.pow(vetor[i].d1 - campo_d1[1], 2) + math.pow(vetor[i].d2 - campo_d2[1], 2))
 
+    # Associando cada objeto ao centroide mais próximo
+    for i in range(len(vetor)):
+        if vetor[i].dist[0] < vetor[i].dist[1]:     # A menor distância é para o primeiro centroide
+            vetor[i].cluster_anterior, vetor[i].cluster_atual = vetor[i].cluster_atual, 0
+        else:       # A menor distância é para o segundo centróide
+            vetor[i].cluster_anterior, vetor[i].cluster_atual = vetor[i].cluster_atual, 1
+    
+    flag = False
 
-campo_d1[0] = campo_d1[0] / qtdd[0]
-campo_d2[0] = campo_d2[0] / qtdd[0]
-campo_d1[1] = campo_d1[1] / qtdd[1]
-campo_d2[1] = campo_d2[1] / qtdd[1]
-
-print 'Centroide 1: (', campo_d1[0], ',', campo_d2[0], ')'
-print 'Centroide 2: (', campo_d1[1], ',', campo_d2[1], ')'
+    # Laço que verifica se algum objeto mudou de centroide
+    for i in range(len(vetor)):
+        if vetor[i].cluster_anterior != vetor[i].cluster_atual:     # Se mudou, marca a flag
+            flag = True
+            break
+    
+    if not flag:    # Se não houve mudança, para o algoritmo
+        break
