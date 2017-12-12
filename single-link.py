e6d8253e695aaade235ca48e871b54d0fe4dfb0a
Ricardo Mendes <ricardomlealjr@gmail.com>
2017-12-12 21:55:05 -0200

    Travamo

--- single-link.py
+++ single-link.py
@@ -30,8 +30,8 @@ vetor = [] #objetos
 
 qtdd_dados = 0
 
-if len(sys.argv) != 5:
-    print 'MODO DE USAR: python single-link.py <entrada.txt> <saida.clu> <quantidade_de_clusters> <quantidade_de_iterações>'
+if len(sys.argv) != 4:
+    print 'MODO DE USAR: python single-link.py <entrada.txt> <saida.clu> <quantidade_de_clusters>'
     exit()
 
 
@@ -60,24 +60,18 @@ for line in arquivo_dados:
 
 #Calcular distancia de um elemento para todos os elementos e inserindo no cluster
 for i in range(len(cluster)):
-    for j in range(len(vetor)):    
+    for j in range(len(vetor)):
         if i > j:
-            break
+            pass
         else: 
             cluster[i].dist.append(math.sqrt(math.pow(vetor[j].d1 - vetor[i].d1 , 2) + math.pow(vetor[j].d2 - vetor[i].d2 , 2)))
 
-# for i in range(0, 10):
-#     print 'dist =', cluster[0].dist[i]
-# sys.exit()
-
 # quais os Objetos mais pertos
-menores_dist=[]
+menores_dist = []
 for i in range(len(cluster)):
     dist = 9999999999999
     k = False
-    # print "i =", i
     for j in range(len(cluster[i].dist)):
-        # print "j =", j
         if cluster[i].dist[j] < dist and j != 0:
             dist = cluster[i].dist[j]
             if not k:
@@ -86,27 +80,69 @@ for i in range(len(cluster)):
             else:
                 menores_dist[i] = [cluster[i].dist[j], i, j]
 
-menores_dist = sorted(menores_dist,key=itemgetter(0))
+menores_dist = sorted(menores_dist, key=itemgetter(0))
 
 #limpando distancias 
 for i in range(len(cluster)):
     cluster[i].dist = []
 
-while qtdd_dados != int(sys.argv[3]):
-    #juntar I E J
-    for i in range(len(menores_dist)):
-        cluster[menores_dist[0][1]].dist.append(menores_dist[0][2])
-        if i == 5:
-            sys.exit()
-        for j in range(len(menores_dist)):
-            if menores_dist[i][2] == menores_dist[j][1]:
-                menores_dist[j][1] = menores_dist[i][1]
+for i in range(len(vetor)):
+    cluster_final = [[i]]
 
-        del cluster[menores_dist[0][2]]
-        del menores_dist[0]
+inseridos = []
 
+for i in range(len(menores_dist)):
+    flag = False
+    for j in range(len(cluster_final)):
+        if menores_dist[i][1] in cluster_final[j] and menores_dist[i][2] in cluster_final[j]:   # Ambos já estão no mesmo cluster
+            flag = True
+            break
+        elif menores_dist[i][1] in cluster_final[j]:    # Só o primeiro está no cluster
+            cluster_final[j].append(menores_dist[i][2]) # Colocamos o segundo no cluster
+            flag = True
+            break
+        elif menores_dist[i][2] in cluster_final[j]:    # Só o segundo está no cluster
+            cluster_final[j].append(menores_dist[i][1]) # Colocamos o primeiro no cluster
+            flag = True
+            break
+
+    if not flag:    # Se Não colocamos nenhum dos dois em nenhum cluster, eles são um novo cluster
+        cluster_final.append([menores_dist[i][1], menores_dist[i][2]])        
+            
+
+# while len(cluster_final) != int(sys.argv[3]):
+#     #juntar I E J
+#     for j in range(len(menores_dist)):
+#         # cluster[menores_dist[0][1]].dist.append(menores_dist[0][2])
+#         pos, inserir = menores_dist[j][1], menores_dist[j][2]       # Para inserir no cluster
+#         for k in range(len(cluster_final)):
+#             # if menores_dist[0][1] == menores_dist[j][1]:
+#             #     menores_dist[j][1] = menores_dist[0][1]
+#             if in cluster_final[k]:  # Se ele estiver em algum cluster, marcamos a flag com True
+#                 if k == pos:    # Se for a posição for a  que eu queria inserir
+#                     break
+#                 else:   # Se não
+#                     pos = k     # Atualizamos a 
+#                     break 
+
+#         cluster[pos].dist.append(inserir)
+    
+#     for j in range(len(cluster)):
+#         if not cluster[i].dist:  # Se o vetor estiver vazio
+#             pass
+#         else:   # Se tiver conteudo
+#             cluster_final = cluster[i].dist     # Copia o conteudo para o final
+
+
+#         print j
+#         print 'cluster =', len(menores_dist)
+#         del cluster[menores_dist[0][2]]
+#         del menores_dist[0]
+#         if j <= i:
+#             break
+        
     #remover o outro cluster
-    qtdd_dados -= 1
+    # qtdd_dados -= 1
 
 final = []
 for i in range(len(cluster)):
@@ -125,12 +161,3 @@ for i in range(len(final)):
 # Junta o mais perto
 # Atualiza a distancia desse Cluster aos outros
 # Continua o Ciclo ate chegar a X interaçoes
-
-
-
-
-
-
-
-
-
